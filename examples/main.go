d8e2601bea2edeb2ac3d4dc30a0956bb47850943
lmxia <xialingming@gmail.com>
2022-01-25 17:06:25 +0800

    add demo files
    Signed-off-by: lmxia <xialingming@gmail.com>

---
+++ examples/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	gaiav1 "gaia.io/gaia/pkg/apis/platform/v1alpha1"
+	"gaia.io/gaia/pkg/generated/clientset/versioned"
+	flag "github.com/spf13/pflag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := versioned.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	targetClient := clientset.PlatformV1alpha1().Targets()
+
+	target := &gaiav1.Target{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "parent-cluster",
+		},
+		Spec: gaiav1.TargetSpec{
+			BootstrapToken: "2b1j4b.ys7qfar6y4fszjd9", // 固定的
+			ParentURL: "https://192.168.1.55:6443",    // 动态获取
+		},
+	}
+
+	result, err := targetClient.Create(context.TODO(), target, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created target %q.\n", result.GetObjectMeta().GetName())
+}

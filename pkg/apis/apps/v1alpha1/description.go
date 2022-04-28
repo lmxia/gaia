bd7d98512f7ca3672d1086e2f895cd5b2f5eff55
ssider <retrieveave@yeah.net>
2022-04-28 10:50:53 +0800

    changed description support serveless

--- pkg/apis/apps/v1alpha1/description.go
+++ pkg/apis/apps/v1alpha1/description.go
@@ -3,6 +3,7 @@ package v1alpha1
 import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/pkg/apis"
 )
 
 // Important: Run "make generated" to regenerate code after modifying this file
@@ -25,25 +26,27 @@ type Description struct {
 
 // DescriptionSpec defines the spec of Description
 type DescriptionSpec struct {
-	// Raw is the underlying serialization of all objects.
-	//
-	//Raw [][]byte `json:"raw,omitempty"`
 	// +required
 	AppID string `json:"appID,omitempty"` // appID是蓝图的id
 	// +optional
 	// +kubebuilder:validation:Optional
-	Component []Components `json:"component,omitempty"`
+	Components []Component `json:"components,omitempty"`
 }
-type Components struct {
-	// +required
-	//AppID string `json:"appID,omitempty"` // appID是蓝图的id
+type Component struct {
 	// +optional
 	Namespace string `json:"namespace,omitempty"`
 	// +required
 	Name string `json:"name,omitempty"`
+
+	// +optional
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:pruning:PreserveUnknownFields
-	Module corev1.PodTemplateSpec `json:"module" protobuf:"bytes,3,opt,name=module"` //module是container意思。
+	Serverless ServerlessSpec `json:"serverless,omitempty" protobuf:"bytes,3,opt,name=serverless"`
+
+	// +optional
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:pruning:PreserveUnknownFields
+	Module corev1.PodSpec `json:"module" protobuf:"bytes,3,opt,name=module"`
 	// +required
 	RuntimeType string `json:"runtimeType,omitempty"`
 	// +required
@@ -64,6 +67,76 @@ type Workload struct {
 	// +optional
 	TraitServerless *TraitServerless `json:"traitServerless,omitempty"`
 }
+type ServerlessSpec struct {
+	// +optional
+	Revision RevisionSpec `json:"revision,omitempty"`
+	// Traffic specifies how to distribute traffic over a collection of
+	// revisions and configurations.
+	// +optional
+	Traffic []TrafficTarget `json:"traffic,omitempty"`
+}
+
+// RevisionSpec holds the desired state of the Revision (from the client).
+type RevisionSpec struct {
+	// ContainerConcurrency specifies the maximum allowed in-flight (concurrent)
+	// requests per container of the Revision.  Defaults to `0` which means
+	// concurrency to the application is not limited, and the system decides the
+	// target concurrency for the autoscaler.
+	// +optional
+	ContainerConcurrency *int64 `json:"containerConcurrency,omitempty"`
+
+	// TimeoutSeconds is the maximum duration in seconds that the request routing
+	// layer will wait for a request delivered to a container to begin replying
+	// (send network traffic). If unspecified, a system default will be provided.
+	// +optional
+	TimeoutSeconds *int64 `json:"timeoutSeconds,omitempty"`
+}
+
+// TrafficTarget holds a single entry of the routing table for a Route.
+type TrafficTarget struct {
+	// Tag is optionally used to expose a dedicated url for referencing
+	// this target exclusively.
+	// +optional
+	Tag string `json:"tag,omitempty"`
+
+	// RevisionName of a specific revision to which to send this portion of
+	// traffic.  This is mutually exclusive with ConfigurationName.
+	// +optional
+	RevisionName string `json:"revisionName,omitempty"`
+
+	// ConfigurationName of a configuration to whose latest revision we will send
+	// this portion of traffic. When the "status.latestReadyRevisionName" of the
+	// referenced configuration changes, we will automatically migrate traffic
+	// from the prior "latest ready" revision to the new one.  This field is never
+	// set in Route's status, only its spec.  This is mutually exclusive with
+	// RevisionName.
+	// +optional
+	ConfigurationName string `json:"configurationName,omitempty"`
+
+	// LatestRevision may be optionally provided to indicate that the latest
+	// ready Revision of the Configuration should be used for this traffic
+	// target.  When provided LatestRevision must be true if RevisionName is
+	// empty; it must be false when RevisionName is non-empty.
+	// +optional
+	LatestRevision *bool `json:"latestRevision,omitempty"`
+
+	// Percent indicates that percentage based routing should be used and
+	// the value indicates the percent of traffic that is be routed to this
+	// Revision or Configuration. `0` (zero) mean no traffic, `100` means all
+	// traffic.
+	// When percentage based routing is being used the follow rules apply:
+	// - the sum of all percent values must equal 100
+	// - when not specified, the implied value for `percent` is zero for
+	//   that particular Revision or Configuration
+	// +optional
+	Percent *int64 `json:"percent,omitempty"`
+
+	// URL displays the URL for accessing named traffic targets. URL is displayed in
+	// status, and is disallowed on spec. URL must contain a scheme (e.g. http://) and
+	// a hostname, but may not contain anything else (e.g. basic auth, url path, etc.)
+	// +optional
+	URL *apis.URL `json:"url,omitempty"`
+}
 
 type TraitDeployment struct {
 	Replicas int32 ` json:"replicas,omitempty"`
@@ -79,6 +152,7 @@ type TraitServerless struct {
 //	// +optional
 //	replicas int32 `json:"replicas,omitempty"` // deploy和serverless确定结构。annity的deamonset trait是空，userapp空
 //}
+
 type SchedulePolicy struct {
 	// +optional
 	SpecificResource *metav1.LabelSelector `json:"specificResource,omitempty"`
--- pkg/generated/listers/platform/v1alpha1/target.go
+++ pkg/generated/listers/platform/v1alpha1/target.go
@@ -26,13 +26,10 @@ import (
 )
 
 // TargetLister helps list Targets.
-// All objects returned here must be treated as read-only.
 type TargetLister interface {
 	// List lists all Targets in the indexer.
-	// Objects returned here must be treated as read-only.
 	List(selector labels.Selector) (ret []*v1alpha1.Target, err error)
 	// Get retrieves the Target from the index for a given name.
-	// Objects returned here must be treated as read-only.
 	Get(name string) (*v1alpha1.Target, error)
 	TargetListerExpansion
 }

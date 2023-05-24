77a77a75fa13728cd5dd5ebd0538738004e909f3
HQing <HQing2010@163.com>
2023-05-24 11:41:51 +0800

    desc render: ExpectedPerformance section and serverless structure
    1. Parse the boundaries and maintenance, tranfer them to components' workload TraitXxx.
    2. Init TraitDeployment and TraitAffinityDaemon.
    3. Update task's completions and statefulset's replicas according to the boundaries.
    4. Get serverless threshold string according to the maintanence's increase or decrease strategy and the boundaries.

--- pkg/utils/render.go
+++ pkg/utils/render.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	lmmserverless "github.com/SUMMERLm/serverless/api/v1"
 	appsv1alpha1 "github.com/lmxia/gaia/pkg/apis/apps/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog/v2"
@@ -41,7 +42,45 @@ func DescToComponents(desc *appsv1alpha1.Description) (components []appsv1alpha1
 	ParseDeploymentCondition(desc.Spec.DeploymentCondition, components, comLocation, affinity)
 
 	// 3. expectedPerformance
-	// TODO
+	ParseExpectedPerformance(desc.Spec.ExpectedPerformance, components, comLocation)
+
+	// 4. Some transformations according to the different workload types.
+	// For sn, transfer it to the specified TtraitXxx.
+	for _, comn := range components {
+		if comn.Workload.Workloadtype == appsv1alpha1.WorkloadTypeUserApp && comn.SchedulePolicy.Level != nil {
+			// use appsv1alpha1.SchedulePolicyMandatory for default
+			for _, exp := range comn.SchedulePolicy.Level[appsv1alpha1.SchedulePolicyMandatory].MatchExpressions {
+				if exp.Key == "sn" { // 还要加上sn.Operator的参考 TODO
+					comn.Workload.TraitUserAPP.SN = exp.Values[0]
+					break
+				}
+			}
+			klog.V(5).Infof("%s's Workload.TraitUserAPP is %+v", comn.Name, comn.Workload.TraitUserAPP)
+		} else if comn.Workload.Workloadtype == appsv1alpha1.WorkloadTypeAffinityDaemon && comn.SchedulePolicy.Level != nil {
+			// use appsv1alpha1.SchedulePolicyMandatory for default
+			for _, exp := range comn.SchedulePolicy.Level[appsv1alpha1.SchedulePolicyMandatory].MatchExpressions {
+				if exp.Key == "sn" { // 还要加上sn.Operator的参考 TODO
+					comn.Workload.TraitAffinityDaemon.SNS = exp.Values
+					break
+				}
+			}
+			klog.V(5).Infof("%s's Workload.TraitAffinityDaemon is %+v", comn.Name, comn.Workload.TraitAffinityDaemon)
+		} else {
+			// for log view.
+			if comn.Workload.TraitStatefulSet != nil {
+				klog.V(5).Infof("%s's Workload.TraitStatefulSet is %+v", comn.Name, comn.Workload.TraitStatefulSet)
+			} else if comn.Workload.TraitTask != nil {
+				if len(comn.Schedule) != 0 {
+					comn.Workload.TraitTask.Schedule = comn.Schedule
+				}
+				klog.V(5).Infof("%s's 'Workload.TraitTask is %+v", comn.Name, comn.Workload.TraitTask)
+			} else if comn.Workload.TraitServerless != nil {
+				klog.V(5).Infof("%s's 'Workload.TraitServerless is %+v", comn.Name, comn.Workload.TraitServerless)
+			} else if comn.Workload.TraitDeployment != nil {
+				klog.V(5).Infof("%s's Workload.TraitDeployment is %+v", comn.Name, comn.Workload.TraitDeployment)
+			}
+		}
+	}
 
 	return components, comLocation, affinity
 }
@@ -138,4 +177,169 @@ func SchedulePolicyReflect(condition appsv1alpha1.Condition, spLevel *metav1.Lab
 	}
 	return spLevel
 
+}
+
+// ParseExpectedPerformance parse desc.Spec.ExpectedPerformance
+func ParseExpectedPerformance(ep appsv1alpha1.ExpectedPerformance, components []appsv1alpha1.Component, comLocation map[string]int) {
+
+	// get boundaries map
+	boundaryMap := ParseBoundaries(ep.Boundaries, components, comLocation)
+	// parse HPA or VPA  and then get the threshold
+	ParseMaintenance(ep.Maintenance, boundaryMap, components, comLocation)
+}
+
+// ParseBoundaries returns a map that the key is the boundary name and the value is the boundary itself
+func ParseBoundaries(boundary appsv1alpha1.Boundaries, components []appsv1alpha1.Component, comLocation map[string]int) map[string]appsv1alpha1.Boundary {
+
+	// expectedPerformance.Boundaries.Inner
+	boundaryMap := make(map[string]appsv1alpha1.Boundary)
+	klog.V(5).Infof("start to parse expectedPerformance.Boundaries.Inner:")
+	for _, inner := range boundary.Inner {
+		klog.V(5).Infof("inner is %+v", inner)
+		index := comLocation[inner.Subject]
+		if "replicas" == inner.Type {
+			var data int32
+			_ = json.Unmarshal(inner.Value, &data)
+			klog.V(5).Infof("%v's replicas is %v", inner.Subject, data)
+			if components[index].Workload.Workloadtype == appsv1alpha1.WorkloadTypeTask {
+				components[index].Workload.TraitTask.Completions = data
+			} else if components[index].Workload.Workloadtype == appsv1alpha1.WorkloadTypeStatefulSet {
+				components[index].Workload.TraitStatefulSet.Replicas = data
+			} else if components[index].Workload.TraitDeployment == nil {
+				// TraitDeployment init
+				components[index].Workload.TraitDeployment = &appsv1alpha1.TraitDeployment{
+					Replicas: data,
+				}
+				klog.V(5).Infof("%s:components[%d].Workload.TraitDeployment.Replicas is %v", inner.Subject, index, components[index].Workload.TraitDeployment.Replicas)
+			}
+
+		} else if "daemonset" == inner.Type {
+			var data bool
+			_ = json.Unmarshal(inner.Value, &data)
+			if data {
+				// TraitAffinityDaemon init
+				components[index].Workload.Workloadtype = appsv1alpha1.WorkloadTypeAffinityDaemon
+				components[index].Workload.TraitAffinityDaemon = &appsv1alpha1.TraitAffinityDaemon{
+					SNS: []string{},
+				}
+			} else {
+				continue
+			}
+		} else {
+			// serverless
+			var data int32
+			_ = json.Unmarshal(inner.Value, &data)
+			components[index].Workload.Workloadtype = appsv1alpha1.WorkloadTypeServerless
+			if "maxReplicas" == inner.Type {
+				components[index].Workload.TraitServerless = &lmmserverless.TraitServerless{
+					MaxReplicas:   data,
+					ResplicasStep: 1, // default 1
+					Threshold:     "",
+				}
+				klog.V(5).Infof("%s:components[%d].Workload.TraitServerless.MaxReplicas is %v", inner.Subject, index, components[index].Workload.TraitServerless.MaxReplicas)
+			} else if "maxQPS" == inner.Type {
+				components[index].Workload.TraitServerless = &lmmserverless.TraitServerless{
+					MaxQPS:    data,
+					QpsStep:   1,
+					Threshold: "",
+				}
+				klog.V(5).Infof("%s:components[%d].Workload.TraitServerless.MaxQPS is %v", inner.Subject, index, components[index].Workload.TraitServerless.MaxQPS)
+			} else {
+				// serverless threshold fields: cpuMax, cpuMin, memMax, memMin, qpsMax, qpsMin
+				boundaryMap[inner.Name] = inner
+			}
+		}
+	}
+
+	// expectedPerformance.Boundaries.Inter
+	klog.V(5).Info("start to parse expectedPerformance.Boundaries.Inter:")
+	for _, _ = range boundary.Inter {
+		// TODO
+	}
+
+	// expectedPerformance.Boundaries.Extra
+	klog.V(5).Info("start to parse expectedPerformance.Boundaries.Extra:")
+	for _, _ = range boundary.Extra {
+		// TODO
+	}
+	return boundaryMap
+}
+
+// ParseMaintenance reflect the boundaries and maintenance's trigger to serverless' threshold.
+func ParseMaintenance(maintenance appsv1alpha1.Maintenance, boundaryMap map[string]appsv1alpha1.Boundary, components []appsv1alpha1.Component, comLocation map[string]int) {
+
+	// 1. expectedPerformance.Maintenance.HPA
+	threshold := make([]map[string]int32, len(components))
+	for i, _ := range threshold {
+		threshold[i] = make(map[string]int32)
+	}
+	klog.V(5).Info("start to parse expectedPerformance.Maintenance.HPA:")
+	for _, hpa := range maintenance.HPA {
+		index := comLocation[hpa.Subject]
+		if components[index].Workload.TraitServerless == nil {
+			klog.Infof("init %s's TraitServerless", hpa.Subject)
+			components[index].Workload.TraitServerless = &lmmserverless.TraitServerless{
+				Threshold: "",
+			}
+		}
+
+		var value, step int32
+		_ = json.Unmarshal(hpa.Strategy.Value, &step)
+		if components[index].Workload.TraitServerless.MaxQPS != 0 {
+			components[index].Workload.TraitServerless.QpsStep = step
+		} else if components[index].Workload.TraitServerless.MaxReplicas != 0 {
+			components[index].Workload.TraitServerless.ResplicasStep = step
+		}
+		if "increase" == hpa.Strategy.Type {
+			// 扩 或 最大值
+			boundaryList := strings.Split(hpa.Trigger, "||")
+			for _, bn := range boundaryList {
+				bn = strings.TrimSpace(bn)
+				_ = json.Unmarshal(boundaryMap[bn].Value, &value)
+				switch boundaryMap[bn].Type {
+				case "cpu":
+					threshold[index]["cpuMax"] = value
+				case "mem":
+					threshold[index]["memMax"] = value
+				case "QPS":
+					threshold[index]["qpsMax"] = value
+				}
+			}
+
+		} else if "decrease" == hpa.Strategy.Type {
+			// 缩 且 最小值
+			boundaryList := strings.Split(hpa.Trigger, "&&")
+
+			for _, bn := range boundaryList {
+				bn = strings.TrimSpace(bn)
+				_ = json.Unmarshal(boundaryMap[bn].Value, &value)
+				switch boundaryMap[bn].Type {
+				case "cpu":
+					threshold[index]["cpuMin"] = value
+				case "mem":
+					threshold[index]["memMin"] = value
+				case "QPS":
+					threshold[index]["qpsMin"] = value
+				}
+			}
+		}
+	}
+
+	// convert the threshold that type is map[string]int32 to string in order to get the serverless threshold string
+	for i, td := range threshold {
+		if len(td) != 0 {
+			tdByte, _ := json.Marshal(td)
+			klog.V(5).Infof("string(tdByte) %s:td is %s", components[i].Name, string(tdByte))
+			components[i].Workload.TraitServerless.Threshold = string(tdByte)
+			klog.V(5).Infof("%s:component[%d] TraitServerless is %+v", components[i].Name, i, components[i].Workload.TraitServerless)
+		}
+	}
+
+	// 2. expectedPerformance.Maintenance.VPA
+	klog.V(5).Info("start to parse expectedPerformance.Maintenance.VPA:")
+	for _, vpa := range maintenance.VPA {
+		index := comLocation[vpa.Subject]
+		components[index].Workload.Workloadtype = appsv1alpha1.WorkloadTypeServerless
+		// TODO
+	}
 }
